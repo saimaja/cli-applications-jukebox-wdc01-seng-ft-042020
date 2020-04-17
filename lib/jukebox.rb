32aace8a69efb48c61d5ba24dd818f3048011b6b
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-17 20:46:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,16 +7,16 @@ def help
   puts "- exit : exits this program"
 end 
   
-def list(songs)
-songs.each_with_index {|song, index|
-  puts "#{index+1}. #{song}"
-}
-end
-
 # def list(songs)
-#   songs.each_with_index do {|song, i| puts "#{i+1}. #{song}"
-#   }
+# songs.each_with_index {|song, index|
+#   puts "#{index+1}. #{song}"
+# }
 # end
+
+def list(songs)
+  songs.each_with_index do {|song, i| puts "#{i+1}. #{song}"
+  }
+end
 
 
 def play
