03e957657998ae63a160e1a647a4daf19f2099f7
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-17 20:27:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -5,8 +5,8 @@ def help
   puts "list : displays a list of songs you can play"
   puts "play : lets you choose a song to play"
   puts "exit : exits this program"
-  help
 end 
+help
   
 def list
 end
