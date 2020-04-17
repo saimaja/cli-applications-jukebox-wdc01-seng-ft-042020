c57dfc740cae1271e952cd5d69c43beedb7c5587
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-17 20:25:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,8 +1,8 @@
 # Add your code here
 def help
-  puts I accept the following commands:
-  puts help : displays this help message
-  puts list : displays a list of songs you can play
+  puts "I accept the following commands:
+  help : displays this help message
+  list : displays a list of songs you can play
   puts play : lets you choose a song to play
   puts exit : exits this program
 end 
